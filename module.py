16ffae752fa96205231bd11245e29d1cc4c599c1
Dolley1997 <59650927+Dolley1997@users.noreply.github.com>
2020-01-21 10:21:06 +0530

    Add files via upload

---
+++ module.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 17 14:11:46 2020
+
+@author: Dolley
+"""
+map={
+     "name":"Dolley",
+     "age":"22",
+     "empId":"589"
+     }
+
+
+reflections = {
+  "i am"       : "you are",
+  "i was"      : "you were",
+  "i"          : "you",
+  "i'm"        : "you are",
+  "i'd"        : "you would",
+  "i've"       : "you have",
+  "i'll"       : "you will",
+  "my"         : "your",
+  "you are"    : "I am",
+  "you were"   : "I was",
+  "you've"     : "I have",
+  "you'll"     : "I will",
+  "your"       : "my",
+  "yours"      : "mine",
+  "you"        : "me",
+  "me"         : "you"
+}
+
+
